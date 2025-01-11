6294df35a510683638be415919e0cb9d54303d0a
Toan Pham <toan.chpham@gmail.com>
2025-01-10 20:26:07 -0600

    Initial Commit

---
+++ PhotoPurge/PhotoPurgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPurgeApp.swift
+//  PhotoPurge
+//
+//  Created by Toan Pham on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPurgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
