424ada0d630d34a6e610aae691c3b9f8d23c81b5
Toan Pham <toan.chpham@gmail.com>
2025-01-21 13:41:39 -0600

    Create Dependency.swift

---
+++ PhotoPurge/Dependency/Dependency.swift
@@ -0,0 +1,54 @@
+//
+//  Dependency.swift
+//  PhotoPurge
+//
+//  Created by Toan Pham on 1/21/25.
+//
+
+import Foundation
+
+class Dependency {
+    let assetService: AssetService
+    
+    init(assetService: AssetService = .init()) {
+        self.assetService = assetService
+    }
+    
+    static let preview: Dependency = .init()
+    
+    class Services {
+        let assetService: AssetService
+        
+        init(dependency: Dependency) {
+            self.assetService = dependency.assetService
+        }
+    }
+    
+    private func services() -> Services {
+        return Services(dependency: self)
+    }
+    
+    class ViewModels {
+        let services: Services
+        
+        init(services: Services) {
+            self.services = services
+        }
+    }
+    
+    private func viewModels() -> ViewModels {
+        return ViewModels(services: services())
+    }
+    
+    class Views {
+        let viewModels: ViewModels
+        
+        init(viewModels: ViewModels) {
+            self.viewModels = viewModels
+        }
+    }
+    
+    private func views() -> Views {
+        return Views(viewModels: viewModels())
+    }
+}
